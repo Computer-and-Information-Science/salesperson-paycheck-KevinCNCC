2cde28e606f4ae1ceba9bc4bdb5437d214aff4d7
KevinCNCC <kevin.crook@student.northampton.edu>
2026-02-04 19:32:39 +0000

    Done except for the file prompt

--- main.cpp
+++ main.cpp
@@ -1,5 +1,30 @@
 #include <iostream>
+#include <fstream>
+#include <iomanip>
 
+using namespace std;
 int main() {
   std::cout << "Hello World!\n";
+
+  ifstream inData;
+  ofstream outData;
+
+  inData.open("sampleinput.txt");
+  outData.open("output.dat");
+
+  string firstName, lastName;
+  double baseSalary, totalCommissions, commissionPercent, expenses, beforeCommission, total;
+  outData << fixed << setprecision(2);
+
+  inData >> firstName >> lastName >> baseSalary >> commissionPercent >> beforeCommission >> expenses;
+  totalCommissions = (beforeCommission*commissionPercent)/100;
+  total = baseSalary + totalCommissions + expenses;
+
+  outData << "Payroll data for " << firstName << " " << lastName << endl;
+  outData << "   " << endl;
+  outData << "Base Salary: " << setw(12) << baseSalary << endl;
+  outData << "Commission: " << setw(13) << totalCommissions << " (" << commissionPercent << "%" << " of " << beforeCommission << ")" << endl;
+  outData << "Expenses: " << setw(15) << expenses << endl;
+  outData << setw(25) << "--------" << endl;
+  outData << "Total: " << setw(18) << total << endl;
 }
